247ba4c63d57df074ebc2c2b3cc81bb68a799d99
Hespt <24688505+Hespt@users.noreply.github.com>
2024-08-08 17:09:41 +0100

    Update ubuntu_installs.sh

--- ubuntu_installs.sh
+++ ubuntu_installs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install -y libreoffice gimp qimgv thunderbird vlc ffmpeg xournalpp htop zsh synaptic build-essential
+sudo apt install -y libreoffice gimp qimgv thunderbird vlc ffmpeg xournalpp htop zsh synaptic build-essential curl wget
